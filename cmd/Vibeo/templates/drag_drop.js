c538c4efa1a3a17225dbd084c9468a6031d22017
AbdulazizAli3 <abdulaziz.ali@softwarfare.com>
2024-04-22 12:09:04 -0500

    added drag and drop mechanisim but need script is not being recognized by go

---
+++ cmd/Vibeo/templates/drag_drop.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var input = document.getElementById("drop_zone");
+  var infoZone = document.getElementById("filename")
+
+
+  input.addEventListener("change", showFileName)
+  input.addEventListener("drop", dropHandler)
+  input.addEventListener("dragover", dragOverHandler)
+
+  function dropHandler(ev) {
+    console.log("File(s) dropped");
+  
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+  
+    if (ev.dataTransfer.items) {
+      // Use DataTransferItemList interface to access the file(s)
+      [...ev.dataTransfer.items].forEach((item, i) => {
+        // If dropped items aren't files, reject them
+        if (item.kind === "file") {
+          if (i === 0){
+            const file = item.getAsFile();
+            if (file.size < 1000000000){ // restrict size to 1 gb
+              infoZone.textContent = 'File name: ' + file.name;
+              console.log(`… file[${i}].name = ${file.name}`);
+              console.log(`… file[${i}].size = ${file.size/(1000000000)} GB`);
+            }
+          }
+          
+        }
+      });
+    } 
+}
+
+function dragOverHandler(ev) {
+    console.log("File(s) in drop zone");
+  
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+  }
+
+
+  function showFileName(event) {
+    var input = event.target;
+    var fileName = input.files[0].name;
+    infoZone.textContent = 'File name: ' + fileName;
+  }
+});
+
+
+console.log("File(s) in drop zone");
